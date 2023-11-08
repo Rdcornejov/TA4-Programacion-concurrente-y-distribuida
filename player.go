6f0a1c2c1d3fabb8bbcd2ef8d5921f02c65c4cd7
Ronaldo Cornejo <66271146+Rdcornejov@users.noreply.github.com>
2023-11-08 17:16:09 -0500

    Update player.go

--- player.go
+++ player.go
@@ -123,3 +123,119 @@ func pierdeTurno() bool {
 	}
 	return false
 }
+
+func turno_jugador(ficha1 chan bool, ficha2 chan bool, ficha3 chan bool, ficha4 chan bool) {
+	var tiro Lanzamiento = lanzarDados()
+	var ind int
+	if !pierdeTurno() {
+		go func() {
+			if fichas[0].meta == false {
+				ficha1 <- true
+			}
+		}()
+		go func() {
+			if fichas[1].meta == false {
+				ficha2 <- true
+			}
+		}()
+		go func() {
+			if fichas[2].meta == false {
+				ficha3 <- true
+			}
+		}()
+		go func() {
+			if fichas[3].meta == false {
+				ficha4 <- true
+			}
+		}()
+
+		select {
+		case <-ficha1:
+			fmt.Printf("(JUEGA FICHA 1)\n")
+			ind = 0
+
+		case <-ficha2:
+			fmt.Printf("(JUEGA FICHA 2)\n")
+			ind = 1
+
+		case <-ficha3:
+			fmt.Printf("(JUEGA FICHA 3)\n")
+			ind = 2
+
+		case <-ficha4:
+			fmt.Printf("(JUEGA FICHA 4)\n")
+			ind = 3
+		}
+
+		go func() {
+			for {
+				select {
+				case <-ficha1:
+				case <-ficha2:
+				case <-ficha3:
+				case <-ficha4:
+					// Descartar elementos del canal
+				default:
+					// El canal está vacío
+					return
+				}
+			}
+		}()
+
+		if tiro.avanzar {
+			fmt.Println("RESULTADO LANZAMIENTO: ", tiro.dadoA+tiro.dadoB)
+			fichas[ind].posicion += tiro.dadoA + tiro.dadoB
+			if fichas[ind].posicion > 39 {
+				fichas[ind].posicion = 39 - (fichas[ind].posicion - 39)
+			}
+		} else {
+			fmt.Println("RESULTADO LANZAMIENTO: ", tiro.dadoA-tiro.dadoB)
+			fichas[ind].posicion += tiro.dadoA - tiro.dadoB
+			if fichas[ind].posicion < 0 {
+				fichas[ind].posicion = 0
+			}
+		}
+		fmt.Println("POSCION ACTUAL DE LA FICHA: ", fichas[ind].posicion)
+		// gano?
+		for i := 0; i < 4; i++ {
+			if fichas[i].posicion == 39 {
+				fichas[i].meta = true
+			}
+		}
+
+	} else {
+		fmt.Println("ESTE JUGADOR PERDIO SU TURNO")
+	}
+	fmt.Println("-------------------------")
+}
+
+func main() {
+
+	br := bufio.NewReader(os.Stdin)
+	fmt.Print("Ingresa el color del jugador: ")
+	color, _ := br.ReadString('\n')
+	color = strings.TrimSpace(color)
+
+	fmt.Print("Puerto Actual: ")
+	strPuertoLocal, _ := br.ReadString('\n')
+	strPuertoLocal = strings.TrimSpace(strPuertoLocal)
+	direccionLocal := fmt.Sprintf("localhost:%s", strPuertoLocal)
+
+	fmt.Print("Puerto Destino: ")
+	strPuertoRemoto, _ := br.ReadString('\n')
+	strPuertoRemoto = strings.TrimSpace(strPuertoRemoto)
+	direccionRemota = fmt.Sprintf("localhost:%s", strPuertoRemoto)
+
+	chFichas := make([]chan bool, NFICHAS)
+
+	for i := range chFichas {
+		chFichas[i] = make(chan bool)
+	}
+
+	ln, _ := net.Listen("tcp", direccionLocal)
+	defer ln.Close()
+	for {
+		con, _ := ln.Accept()
+		go manejador(con, color, chFichas)
+	}
+}
